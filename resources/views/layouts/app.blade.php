f5761fa8eeff65471fa45c2bfb15174884154265
vlada <vladapantic@hotmail.com>
2022-05-24 11:17:10 +0200

    feature252/Create main layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
+        rel="stylesheet" />
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+
+    <style>
+        body {
+            font-family: "Nunito", sans-serif;
+        }
+    </style>
+</head>
+
+<body class="antialiased">
+    <div
+        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        @if (Route::has('login'))
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">
+                        Home
+                    </a>
+                @else
+                    <a
+                        href="{{ route('login') }}"
+                        class="text-sm text-gray-700 dark:text-gray-500 underline">
+                        Log in
+                    </a>
+                    @if (Route::has('register'))
+                        <a
+                            href="{{ route('register') }}"
+                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">
+                            Register
+                        </a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 bg-slate-200">
+
+            {{ $slot }}
+
+            <div
+                class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                <div
+                    class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                    Laravel v{{ Illuminate\Foundation\Application::VERSION }}
+                    (PHP v{{ PHP_VERSION }})
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './storage/frameworks/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("./plugin")],
+}
